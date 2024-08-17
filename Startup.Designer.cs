5703b372be0f81a6fa72f95b994f1fbbd39026ad
kkrisztofer08 <79632195+kkrisztofer08@users.noreply.github.com>
2024-08-17 19:34:52 +0200

    Add files via upload

---
+++ Startup.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace FileExplorer
+{
+    partial class Startup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Startup));
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.ForeColor = Color.FromArgb(128, 255, 128);
+            progressBar1.Location = new Point(199, 275);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(274, 23);
+            progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(305, 257);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Lemon", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(59, 120);
+            label2.Name = "label2";
+            label2.Size = new Size(583, 103);
+            label2.TabIndex = 2;
+            label2.Text = "Simple File Explorer";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(592, 360);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Copyright © 2024";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Startup
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Pink;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(714, 392);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Startup";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Simple File Explorer";
+            Load += Startup_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ProgressBar progressBar1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileExplorer
+{
+    public partial class Startup : Form
+    {
+        Random random = new Random();
+
+        public Startup()
+        {
+            InitializeComponent();
+        }
+
+        private void Startup_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int randomIncrement = random.Next(1, 10);
+            
+
+            if (progressBar1.Value + randomIncrement >= progressBar1.Maximum)
+            {
+                progressBar1.Value = progressBar1.Maximum;
+                timer1.Stop();
+
+                this.Hide();
+                Form1 form = new Form1();
+                form.Show();
+            }
+            else
+            {
+                progressBar1.Value += randomIncrement;
+                label1.Text = "Loading... " + progressBar1.Value.ToString() + "%";
+            }
+        }
+    }
+}
